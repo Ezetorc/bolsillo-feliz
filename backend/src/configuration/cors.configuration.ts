f7ede5e7e412046a7538415cbcef0fc517fc658b
Mabeliris <mabellecfernandez@gmail.com>
2025-11-06 16:36:18 -0300

    modificar url en todo el proyecto

--- backend/src/configuration/cors.configuration.ts
+++ backend/src/configuration/cors.configuration.ts
@@ -1,8 +1,7 @@
 import { CorsOptions } from "cors";
 
 export const CORS_CONFIGURATION: CorsOptions = {
-  origin: ["http://localhost:5173",
-     "https://bolsillo-feliz-jet.vercel.app",
+  origin: ["https://bolsillo-feliz-jet.vercel.app",
   ],
   methods: ["GET", "POST", "PATCH", "DELETE", "PUT"],
   credentials: true,
